a7d0418500d6fc3e520470cb6b903ec17d26db2c
Rafael Medeiros <rafaelgurudesign@gmail.com>
2021-06-27 19:49:20 -0300

    add get event

---
+++ event.go
@@ -0,0 +1,92 @@
+package bankly
+
+import (
+	"fmt"
+	"net/url"
+	"strconv"
+	"time"
+)
+
+// Transfer is a structure manager all about bankslip
+type Event struct {
+	client *Bankly
+}
+
+type EventGetRequest struct {
+	Branch         string     `json:"branch"`
+	Account        string     `json:"account"`
+	Page           string     `json:"page"`
+	PageSize       string     `json:"pageSize"`
+	IncludeDetails bool       `json:"includeDetails"`
+	BeginDateTime  *time.Time `json:"beginDateTime"`
+	EndDateTime    *time.Time `json:"endDateTime"`
+	CorrelationID  string     `json:"-"`
+}
+
+type EventResponse []*EventItemResponse
+
+type EventItemResponse struct {
+	AggregateId    string       `json:"aggregateId"`
+	Type           string       `json:"type"`
+	Category       string       `json:"category"`
+	DocumentNumber string       `json:"documentNumber"`
+	BankBranch     string       `json:"bankBranch"`
+	NankAccount    string       `json:"bankAccount"`
+	Amount         float32      `json:"amount"`
+	Name           string       `json:"name"`
+	Timestamp      *time.Time   `json:"timestamp"`
+	Data           *interface{} `json:"data"`
+}
+
+type EventAccount struct {
+	Agency     string `json:"Agency"`
+	Account    string `json:"Account"`
+	Document   string `json:"Document"`
+	IspbNumber string `json:"IspbNumber"`
+	Name       string `json:"Name"`
+}
+
+type EventDataExternalTransferResponse struct {
+	ExternalTransactionId string
+	DepositTransactionId  string
+	ControlNumber         string
+	TransactionAmount     float32
+	ClearingAmount        float32
+	OverLimitAmount       float32
+	Channel               string
+	SenderAccount         *EventAccount `json:"SenderAccount"`
+	RecipientAccount      *EventAccount `json:"RecipientAccount"`
+	CorrelationID         string        `json:"CorrelationId"`
+	Document              string        `json:"document"`
+	CompanyKey            string        `json:"CompanyKey"`
+}
+
+//Event - Instance de vvent
+func (c *Bankly) Event() *Event {
+	return &Event{client: c}
+}
+
+func (a *Event) Get(req *EventGetRequest) (*EventResponse, *Error, error) {
+	var response *EventResponse
+	params := url.Values{}
+	params.Add("account", req.Account)
+	params.Add("branch", req.Branch)
+	params.Add("page", req.Page)
+	params.Add("pageSize", req.PageSize)
+	params.Add("includeDetails", strconv.FormatBool(req.IncludeDetails))
+	if req.BeginDateTime != nil {
+		params.Add("BeginDateTime", req.BeginDateTime.String())
+	}
+	if req.EndDateTime != nil {
+		params.Add("EndDateTime", req.EndDateTime.String())
+	}
+
+	err, errApi := a.client.Request("GET", fmt.Sprintf("events?%s", params.Encode()), req.CorrelationID, nil, &response)
+	if err != nil {
+		return nil, nil, err
+	}
+	if errApi != nil {
+		return nil, errApi, nil
+	}
+	return response, nil, nil
+}
